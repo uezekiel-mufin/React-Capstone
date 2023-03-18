608123bb15dafcedfcdaaf8374e7e865ac845800
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-03-18 15:25:34 +0100

    add navigate back

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { MdArrowBackIosNew, MdKeyboardVoice, MdSettings } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const title = useSelector((state) => state.menu.title);
+  const navigate = useNavigate();
+
   return (
     <div className="text-white flex justify-between w-[398px] items-center px-3 py-5  flex-1 shadow-2xl border border-rose-800 border-solid">
-      <Link to="..">
+      <button type="button" onClick={() => navigate(-1)}>
         <MdArrowBackIosNew className="h-5 w-5" />
-      </Link>
+      </button>
       <h2 className="text-2xl">{title}</h2>
       <div className="flex gap-6">
         <span>
